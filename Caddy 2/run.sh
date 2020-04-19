edcfeaeee10b8e2dd117c6b9ede772a5c1c32cfe
berichta <30439938+berichta@users.noreply.github.com>
2020-04-19 15:10:49 +0200

    Update run.sh
    Added fallback

--- Caddy 2/run.sh
+++ Caddy 2/run.sh
@@ -4,6 +4,15 @@ set -e
 CONFIG_PATH="/data/options.json"
 CADDY_SHARE_PATH="/share/caddy"
 DEFAULT_ARGS=( "--config" "$CADDY_SHARE_PATH/Caddyfile" )
+FALLBACK_DEFAULT_ARGS=( "--config" "/etc/caddy/Caddyfile")
 readarray -t ARGS < <(jq --raw-output '.args[]' $CONFIG_PATH)
 
-caddy run ${DEFAULT_ARGS[*]} ${ARGS[*]}
+if [ -f "$CADDY_SHARE_PATH/Caddyfile" ]; then
+    caddy run ${DEFAULT_ARGS[*]} ${ARGS[*]}
+else
+    export EMAIL="$(jq --raw-output '.email' $CONFIG_PATH)"
+    export DOMAIN="$(jq --raw-output '.domain' $CONFIG_PATH)"
+    export DESTINATION="$(jq --raw-output '.destination' $CONFIG_PATH)"
+    export PORT="$(jq --raw-output '.port' $CONFIG_PATH)"
+    caddy run ${FALLBACK_DEFAULT_ARGS[*]} ${ARGS[*]}
+fi
