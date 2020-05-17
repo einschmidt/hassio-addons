40a033fcb979e17ef77174c0f9b98c3835afec0d
berichta <30439938+berichta@users.noreply.github.com>
2020-05-17 11:31:40 +0200

    Update caddy.sh

--- Caddy 2/rootfs/usr/bin/caddy.sh
+++ Caddy 2/rootfs/usr/bin/caddy.sh
@@ -5,43 +5,43 @@
 # Launch Caddy
 # ------------------------------------------------------------------------------
 non_caddyfile_config() {
-    local EMAIL
-    local DOMAIN
-    local DESTINATION
-    local PORT
-    local ARGS
-
     bashio::log.trace "${FUNCNAME[0]}"
 
     export EMAIL=$(bashio::config 'non_caddyfile_config.email')
     export DOMAIN=$(bashio::config 'non_caddyfile_config.domain')
     export DESTINATION=$(bashio::config 'non_caddyfile_config.destination')
     export PORT=$(bashio::config 'non_caddyfile_config.port')
-    ARGS=$(bashio::config 'args')
-
-    caddy run --config /etc/caddy/Caddyfile ${ARGS}
-}
-
-caddyfile_config() {
-    local ARGS
-
-    bashio::log.trace "${FUNCNAME[0]}"
-
-    ARGS=$(bashio::config 'args')
-
-    caddy run --config /share/caddy/Caddyfile ${ARGS}
+    CONFIG_PATH=/etc/caddy/Caddyfile
 }
 
 main() {
     bashio::log.trace "${FUNCNAME[0]}"
+
+    ARGS=$(bashio::config 'args')
+
+    if bashio::fs.file_exists '/share/caddy/caddy'; then
+        CADDY_PATH=/share/caddy/caddy
+
+        bashio::log.info "Found custom Caddy"
+
+        ${CADDY_PATH} version
+    else
+        CADDY_PATH=/usr/bin/caddy
+
+        bashio::log.info "Use built-in Caddy"
+
+        ${CADDY_PATH} version
+    fi
     
     if bashio::fs.file_exists '/share/caddy/Caddyfile'; then
         bashio::log.info "Caddyfile found"
-        caddyfile_config
+        CONFIG_PATH=/share/caddy/Caddyfile
     else
         bashio::log.info "No Caddyfile found"
         bashio::log.info "Use non_caddyfile_config"
         non_caddyfile_config
     fi
+
+    ${CADDY_PATH} run --config ${CONFIG_PATH} ${ARGS}
 }
 main "$@"
